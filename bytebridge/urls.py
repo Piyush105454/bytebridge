78032df5084b3592be8e97e1a394d3463fda1b3a
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-29 21:17:45 +0530

    Update urls.py

--- bytebridge/urls.py
+++ bytebridge/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from task2 import views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,3 +10,8 @@ urlpatterns = [
      path('', include('task2.urls')),  # Make sure this line is present
    
 ]
+
+# Serve static and media files correctly in production
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
