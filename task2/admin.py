8b58c6f86b7de54dd6cc726d759967cf8ccd5563
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-29 16:16:02 +0530

    Update admin.py

--- task2/admin.py
+++ task2/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Registration
 
-# Register your models here.
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'checked_in')  # Show these columns in admin
+    search_fields = ('email', 'name')  # Search bar for name & email
+    list_filter = ('checked_in',)  # Filter users by check-in status
+
+admin.site.register(Registration, RegistrationAdmin)
