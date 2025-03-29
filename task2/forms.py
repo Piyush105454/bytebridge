5eb8787558bbabdca2cd72d8f71efabd2baa8f68
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-29 16:15:26 +0530

    Update forms.py

--- task2/forms.py
+++ task2/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from .models import Registration
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = Registration
-        fields = ['name', 'email']
-        widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Enter your name'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email'}),
-        }
+        fields = ['name', 'email', 'password']
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
