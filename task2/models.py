562290513484e2d197bcd8c547d36263d39604da
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-29 16:15:00 +0530

    Update models.py

--- task2/models.py
+++ task2/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class Registration(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255, default=make_password("defaultpassword"))  # Temporary default password
     checked_in = models.BooleanField(default=False)
     registered_at = models.DateTimeField(auto_now_add=True)
 
