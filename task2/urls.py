cc4b783bc6443a2e7d2d4579ef7f7c99bcc925eb
Piyush Tamoli <piyushmodi812@gmail.com>
2025-04-23 08:15:17 +0530

    Update urls.py

--- task2/urls.py
+++ task2/urls.py
@@ -1,12 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from .views import check_in, check_out, register, login_view, user_logout
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    path('', register, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('check-in/<str:email>/', check_in, name='check_in'),
-    path('check-out/<str:email>/', check_out, name='check_out'),
+    path('Signup/', views.Signup, name='Signup'),
+    path('verify-email/<uidb64>/<token>/', views.verify_email, name='verify_email'),
 ]
