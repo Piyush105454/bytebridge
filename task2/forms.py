807d6bfbde7cb598bc1bd1726a14d6f21bb3fa3e
Piyush Tamoli <piyushmodi812@gmail.com>
2025-04-23 08:13:30 +0530

    Update forms.py

--- task2/forms.py
+++ task2/forms.py
@@ -1,12 +1,38 @@
 from django import forms
-from .models import Registration
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
-class RegistrationForm(forms.ModelForm):
+User = get_user_model()
+
+class CitizenSignupForm(forms.ModelForm):
+    digilocker_number = forms.CharField(max_length=12)
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
-        model = Registration
-        fields = ['name', 'email', 'password']
-class LoginForm(forms.Form):
-    email = forms.EmailField()
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists.")
+        return email
+
+
+class AdminSignupForm(forms.ModelForm):
+    admin_id = forms.CharField(max_length=10)
     password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists.")
+        return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
