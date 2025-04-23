b75d279c795f3c6c324dab90c559148841fdd4dd
Piyush Tamoli <piyushmodi812@gmail.com>
2025-04-23 08:14:25 +0530

    Update models.py

--- task2/models.py
+++ task2/models.py
@@ -1,12 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.conf import settings
 
-class Registration(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255, default=make_password("defaultpassword"))  # Temporary default password
-    checked_in = models.BooleanField(default=False)
-    registered_at = models.DateTimeField(auto_now_add=True)
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('citizen', 'Citizen'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    email_verified = models.BooleanField(default=False)
+
+    # Extra fields
+    digilocker_number = models.CharField(max_length=50, blank=True, null=True)
+    admin_id = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return self.username
+
+class UserLoginAttempt(models.Model):
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    successful = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Login attempt for {self.user.username} on {self.timestamp}"
