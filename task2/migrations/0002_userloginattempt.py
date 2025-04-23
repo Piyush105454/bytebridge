2a60c69e9400f9c5e0f72848f3023ad8e19e96bf
Piyush Tamoli <piyushmodi812@gmail.com>
2025-04-23 08:50:46 +0530

    Update 0002_userloginattempt.py

--- task2/migrations/0002_userloginattempt.py
+++ task2/migrations/0002_userloginattempt.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Connect', '0001_initial'),
+        ('task2', '0001_initial'),
     ]
 
     operations = [
